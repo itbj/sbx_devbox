bb709efa0e62e411ea91c896a27a4c825412cff9
hpreston <hank.preston@gmail.com>
2018-07-20 09:04:36 -0400

    added sync script

---
+++ tools/devbox_sync.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# This bash script is downloaded and ran on the DevNet DevBox automatially
+# whenever a terminal starts up.  It's purpose is to all the DevBox to get
+# updates and changes to maintain the latest requirements and tools.
+
+# The "SYNC_VERSION" is used to determine whether any pod updates are needed
+SYNC_VERSION=1
+
+# Step 1. Check to see if script needs to run.  If not, exit.
+CURRENT_VERSION=$(<~/sync_version)
+echo "Current SYNC_VERSION ${CURRENT_VERSION}"
+
+if [ "$CURRENT_VERSION" -eq "$SYNC_VERSION" ];
+  then
+    echo "Pod up to date."
+    exit
+fi
+
+echo "Pod configuration needs updates."
+
+# Step 2. Run Updates
+# Enter needed update commands below
+
+# Step 3. Update Pod Version
+echo "Updating SYNC_VERSION to ${SYNC_VERSION}"
+echo ${SYNC_VERSION} > ~/sync_version
